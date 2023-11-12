d67746294b284fd86b47c9b0b30ebc9a30245a47
rehman tyagi <irontyagi4@gmail.com>
2023-11-12 20:34:12 +0530

    added fixed nav on small

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,12 +4,31 @@ import Logo from "../UI/Logo/Logo"
 import Navbar from "../UI/Navbar/Navbar"
 import Button from "../UI/Button/Button"
 import { BiMenuAltRight } from 'react-icons/bi'
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 const Header = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false)
-    console.log(isMenuOpen)
+    const navbarRef = useRef(null)
+    const [y, setY] = useState(20);
+
+    const handleNavigation = (e) => {
+        const window = e.currentTarget;
+        if (y > window.scrollY) {
+            setY(0)
+            navbarRef.current.classList.remove(style.scrollActive)
+        } else if (y < window.scrollY) {
+            console.log("scrolling down");
+            navbarRef.current.classList.add(style.scrollActive)
+        }
+        setY(window.scrollY);
+    };
+
+    useEffect(() => {
+        setY(window.scrollY);
+
+        window.addEventListener("scroll", (e) => handleNavigation(e));
+    }, []);
     return (
-        <div className={style.container}>
+        <div ref={navbarRef} className={style.container}>
             <CenterContent className={style.header}>
                 <Logo />
                 <Navbar className={`${style.navbar} ${isMenuOpen ? style.NavbarActive : ''}`} />
